ee866a1505f45afeddca2b61be8ea23137a3bfda
Dariusz Okonski <darek200180@gmail.com>
2020-03-01 12:52:24 +0100

    add: connnection and model for Blog, create route /blogs to withdraw data form db and send it to blogs.ejs

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+// MongoDB Configuration ==========================
+mongoose.connect('mongodb://localhost/node_blog', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  content: String,
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
 
 // Configuration ==================================
 app.set('view engine', 'ejs');
@@ -11,8 +30,17 @@ app.get('/', (req, res) => {
 });
 
 app.get('/blogs', (req, res) => {
-  res.render('blogs');
-})
+  Blog.find((err, foundedPosts) => {
+    if (err) {
+      res.redirect('/');
+    } else {
+      res.render('blogs', {
+        posts: foundedPosts
+      });
+    }
+  })
+
+});
 
 
 
